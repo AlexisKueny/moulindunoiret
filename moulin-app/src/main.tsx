53685a3c37cb3abd2061494797dcfa955bb3e5fc
Alex <alexis.kueny@outlook.com>
2025-11-01 13:50:24 +0100

    Browser router setup

--- moulin-app/src/main.tsx
+++ moulin-app/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<h1>HOME</h1>} />
+          <Route path="historique" element={<h1>Historique</h1>} />
+          <Route path="projet" element={<h1>Projet</h1>} />
+          <Route path="travaux" element={<h1>Travaux</h1>} />
+          <Route path="*" element={<h1>Page non trouvée</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
