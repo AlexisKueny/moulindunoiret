eb7b396ab7bd1dbe8a411b3b43e9a30182a047b0
Alex <alexis.kueny@outlook.com>
2025-11-10 15:56:22 +0100

    deploy fix

--- moulin-app/src/App.tsx
+++ moulin-app/src/App.tsx
@@ -1,10 +1,8 @@
-import { useTheme } from '@mui/material'
 import Header from './common/Header.tsx'
 import Footer from './common/Footer.tsx'
 import { Outlet } from 'react-router'
 
 const App = () => {
-  const theme = useTheme()
 
   return (
     <div
