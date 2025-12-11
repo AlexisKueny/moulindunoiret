827dbf4fc3a45fbdfc57043c4aa15cce8a754f23
Alex <alexis.kueny@outlook.com>
2025-12-11 17:04:38 +0100

    Creation des cartes pour afficher les photos sur le coté

--- moulin-app/src/common/MapScierie.tsx
+++ moulin-app/src/common/MapScierie.tsx
@@ -1,10 +1,16 @@
 import ScierieMap from "../assets/ScierieMap.png"
 import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
 import { IconButton } from "@mui/material";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 interface MapButtonProps {
     x: number;
     y: number;
+}
+
+interface InfoCardProps {
+    position: string;
 }
 
 const MapButton = ({ x, y }: MapButtonProps) => {
@@ -21,15 +27,32 @@ const MapButton = ({ x, y }: MapButtonProps) => {
     );
 };
 
+const InfoCard = ({position}: InfoCardProps) => {
+    return (
+        <Card>
+            <CardContent>
+                <h2>Je suis une carte {position}</h2>
+                <p>Contenu de ma carte</p>
+            </CardContent>
+        </Card>
+    )
+}
+
 const MapScierie = () => {
     return (
-        <div style={{ marginBottom: "30px", display: "flex", justifyContent: "center", position: "relative" }}>
-            <img
-                src={ScierieMap}
-                alt="carte scierie"
-                style={{ display: "block", maxHeight: "700px", width: "auto", height: "auto" }}
-            />
-            <MapButton x={277} y={590} />
+        <div
+            style={{display:"flex"}}
+        >
+            <InfoCard position="left"/>
+            <div style={{ marginBottom: "30px", display: "flex", justifyContent: "center", position: "relative" }}>
+                <img
+                    src={ScierieMap}
+                    alt="carte scierie"
+                    style={{ display: "block", maxHeight: "700px", width: "auto", height: "auto" }}
+                />
+                <MapButton x={277} y={590} />
+            </div>
+            <InfoCard position="right"/>
         </div>
     )
 }
