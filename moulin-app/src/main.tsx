e77dee308c7be22ad67c8f1691002dbf14b981ad
Alex <alexis.kueny@outlook.com>
2026-01-18 20:49:51 +0100

    redeploy

--- moulin-app/src/main.tsx
+++ moulin-app/src/main.tsx
@@ -23,7 +23,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CssBaseline >
       <ThemeProvider theme={theme}>
-        <BrowserRouter basename={import.meta.env.DEV ? '/' : '/moulindunoiret/'}>
+        <BrowserRouter>
           <Routes>
             <Route element={<App />}>
               <Route index element={<Home />} />
