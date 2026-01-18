17f5e35677a0c6409d2e5d3f6f43f5e2f256ab9e
Alex <alexis.kueny@outlook.com>
2026-01-18 21:02:07 +0100

    Redeploy

--- moulin-app/src/main.tsx
+++ moulin-app/src/main.tsx
@@ -25,25 +25,22 @@ createRoot(document.getElementById('root')!).render(
     <CssBaseline >
       <ThemeProvider theme={theme}>
         <BrowserRouter>
-          <Routes>
-            <Route element={<App />}>
-              {import.meta.env.PROD ? (
-                <>
-                  <Route index element={<WorkInProgress />} />
-                  <Route path="*" element={<WorkInProgress />} />
-                </>
-              ) : (
-                <>
-                  <Route index element={<Home />} />
-                  <Route path="historique" element={<History/>} />
-                  <Route path="projet" element={<h1>Projet</h1>} />
-                  <Route path="travaux" element={<Work/>} />
-                  <Route path="newsletter" element={<h1>Newsletter</h1>} />
-                  <Route path="*" element={<h1>Page non trouvée</h1>} />
-                </>
-              )}
-            </Route>
-          </Routes>
+          {import.meta.env.PROD ? (
+            <Routes>
+              <Route path="*" element={<WorkInProgress />} />
+            </Routes>
+          ) : (
+            <Routes>
+              <Route element={<App />}>
+                <Route index element={<Home />} />
+                <Route path="historique" element={<History/>} />
+                <Route path="projet" element={<h1>Projet</h1>} />
+                <Route path="travaux" element={<Work/>} />
+                <Route path="newsletter" element={<h1>Newsletter</h1>} />
+                <Route path="*" element={<h1>Page non trouvée</h1>} />
+              </Route>
+            </Routes>
+          )}
         </BrowserRouter>
       </ThemeProvider>
     </CssBaseline>
